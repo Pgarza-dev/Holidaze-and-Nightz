d34426c5d698c0f08bc1e7626f3bcf6d21e47350
Pablo Gaza <p.garza1983@gmail.com>
2024-05-10 10:29:06 +0200

    chore: Update profile page with new avatar upload button and additional user information

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import Container from '@/components/Container'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import React from "react";
+import Container from "@/components/Container";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 
 function ProfilePage() {
   return (
     <div className="h-full w-full p-10">
-      <Container className="border-2 border-customBlack h-full w-full p-5">
-        <div className="p-4">
-          <Avatar className="h-20 w-20 md:h-1/4 md:w-1/4">
+      <Container className="h-full w-full border-2 border-customBlack p-5">
+        <div className="p-4 flex flex-col justify-center">
+          <Avatar className="h-full w-full md:h-1/4 md:w-1/4 m-auto">
             <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback>PG</AvatarFallback>
           </Avatar>
-          <h1>Profile name</h1>
+          <Button className="mt-4">Upload new avatar</Button>
         </div>
-        <div></div>
+        <div>
+          <h1>Pablo Garza</h1>
+          <p>Bio here</p>
+          <p>My venues</p>
+          <p>My Bookings</p>
+        </div>
       </Container>
     </div>
-  )
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
