051334cdb9b4c676449f85e458c597729d2c71cf
Pablo Gaza <p.garza1983@gmail.com>
2024-05-28 14:36:02 +0200

    chore: Update NavBar component to fix toggleNavbar function and remove unused code

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,45 +1,24 @@
 "use client";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, use } from "react";
 import Link from "next/link";
 import { ThemeToggle } from "./ThemeToggle";
 import { IoIosSearch } from "react-icons/io";
 import Container from "@/components/Container";
 import SearchBar from "@/components/SearchBar";
-
+import { GrClose } from "react-icons/gr";
 function NavBar() {
   const [isClicked, setisClicked] = useState(false);
-  const menuNav = useRef(null);
 
   const toggleNavbar = () => {
-    setisClicked((prevIsClicked) => !prevIsClicked);
-    const menu = menuNav.current as HTMLElement | null;
-    if (menu) {
-      menu.style.display = isClicked ? "none" : "block";
-    }
+    setisClicked(!isClicked);
   };
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      menuNav.current &&
-      !(menuNav.current as HTMLElement).contains(event.target as Node)
-    ) {
-      setisClicked(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
 
   return (
     <div className="relative w-full bg-customBlack">
       <Container className="relative z-10 w-full font-libre ">
         <header>
           <nav className="relative flex flex-row items-center justify-between">
-            <div ref={menuNav} className="flex flex-col gap-4">
+            <div className="flex flex-col gap-4">
               <button
                 className="text-base text-background hover:text-secondary dark:text-darkText md:text-3xl"
                 onClick={toggleNavbar}
@@ -60,10 +39,11 @@ function NavBar() {
       </Container>
       {isClicked && (
         <div className="absolute top-0 z-50 flex h-screen w-96 flex-col justify-start gap-2 bg-background ps-20 pt-40 font-libre ">
-          <button
-            className="pb-8 text-left text-2xl text-darkText underline underline-offset-auto hover:opacity-50 dark:text-darkText md:text-7xl"
-            onClick={toggleNavbar}
-          >
+          <div onClick={toggleNavbar} className="flex w-full justify-end p-4">
+            <GrClose className="h-6 w-6 cursor-pointer hover:opacity-50" />
+          </div>
+
+          <button className="pb-8 text-left text-2xl text-darkText underline underline-offset-auto hover:opacity-50 dark:text-darkText md:text-7xl">
             Menu
           </button>
           <Link
